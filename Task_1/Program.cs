c5d7b6a5adaa6ca9ee4e195d6529ef4abac9864f
AntonBelov <asbelov1907@gmail.com>
2022-04-25 20:56:16 +0300

    Добавили нахождение квадрата числа

---
+++ Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Принимает число и выдает его квадрат
+//Console.WriteLine("Hello, World!");
+
+
+Console.Write("Введите число -> ");
+int num = int.Parse(Console.ReadLine());
+
+int numResult = num * num;
+Console.Write("Квадрат числа -> ");
+Console.WriteLine(numResult);
+Console.ReadKey();
