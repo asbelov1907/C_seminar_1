36c3281bf5b5f8f95a3916793faf93172856ae54
AntonBelov <asbelov1907@gmail.com>
2022-04-26 07:59:45 +0300

    Добавлены доп задачи

---
+++ Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+
+Console.Write("Введите число А ->");
+
+int numA = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число B ->");
+
+int numB = int.Parse(Console.ReadLine());
+
+int numResult = numB * numB;
+
+if(numResult == numA)
+{
+   Console.Write("Число B является квадратом числа А"); 
+}
+else
+{
+Console.Write("Число B не является квадратом числа А");
+}
---
+++ Task_3/Program.cs
@@ -0,0 +1,60 @@
+﻿
+Console.Write("Введите номер дня недели -> ");
+
+int num = int.Parse(Console.ReadLine());
+
+
+if(num == 1)
+{
+   Console.Write("Сегодня понедельник!"); 
+}
+else
+{
+   if(num == 2)
+{
+   Console.Write("Сегодня вторник"); 
+}
+else
+{
+    if(num == 3)
+{
+   Console.Write("Сегодня среда"); 
+} 
+else
+{
+    if(num == 4)
+{
+   Console.Write("Сегодня четверг"); 
+} 
+
+else
+{
+    if(num == 5)
+{
+   Console.Write("Сегодня пятница"); 
+} 
+
+else
+{
+    if(num == 6)
+{
+   Console.Write("Сегодня себбота"); 
+} 
+
+else
+{
+    if(num == 7)
+{
+   Console.Write("Сегодня воскресенье"); 
+} 
+else
+
+{
+   Console.Write("Нет такого числа"); 
+} 
+}
+}
+}
+}
+}
+}
